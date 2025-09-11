4568d3898e0c7b1622050b5ae563379679f3f5eb
Hazem Khaled <hazem.khaled@gmail.com>
2025-09-11 18:42:59 +0300

    refactor: update incremental cache configuration and remove unused durable objects

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,11 +1,11 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
+import staticAssetsIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/static-assets-incremental-cache";
 import d1NextTagCache from "@opennextjs/cloudflare/overrides/tag-cache/d1-next-tag-cache";
-import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
+// import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
 
 export default defineCloudflareConfig({
-  incrementalCache: r2IncrementalCache,
-  queue: doQueue,
+  incrementalCache: staticAssetsIncrementalCache,
+  // queue: doQueue,
   // This is only required if you use On-demand revalidation
   tagCache: d1NextTagCache,
   // If you don't use `revalidatePath`, you can also filter internal soft tags using the `softTagFilter`
